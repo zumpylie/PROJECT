cbe6394eb1ae074ec36298175288f0bbbd37c962
zumpylie <basbasjunliecerenio@gmail.com>
2026-08-30 18:31:16 +0800

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,14 @@
+console.log("Hello, DSA!");
+
+let score = 90;
+score = 95;
+const student_name = "June";
+console.log(score);
+console.log(student_name);
+
+let age = 20;
+let price = 99.50;
+let student = "Lie";
+let passed = true;
+console.log(typeof age);
+console.log(typeof student);
